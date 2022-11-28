2d5c4d4f57781983642a2c0e91d406d2c8b662a5
jcpsantos <juancaiops@gmail.com>
2022-11-27 21:09:53 -0300

    melhorias na função de agrupamento e criação da função typed_columns

--- sparta/delete.py
+++ sparta/delete.py
@@ -1,5 +1,7 @@
 from azure.storage.blob import ContainerClient
 from sparta.logs import getlogger
+from time import time
+from datetime import timedelta
 
 def delete_blob(dir_name:str, container_name:str, connect:str) -> None:
     """Function to delete files in a blob.
@@ -9,10 +11,15 @@ def delete_blob(dir_name:str, container_name:str, connect:str) -> None:
         container_name (str): Container name.
         connect (str): Blob connection string.
     """
+    logger = getlogger('delete_blob')
+    
+    start_time = time()
+    
     container_client = ContainerClient.from_connection_string(conn_str=connect, container_name=container_name)
     for blob in container_client.list_blobs(name_starts_with=dir_name):
         try:
             container_client.delete_blob(blob.name)
+            logger.info(f"Deleted {blob.name}")
+            logger.info(f"Execution time: {timedelta(seconds = time()-start_time)}")
         except Exception:
-            logger = getlogger('delete_blob')
             logger.error(f"{blob.name} not found")
--- sparta/transformation.py
+++ sparta/transformation.py
@@ -3,6 +3,9 @@ from pyspark.sql.window import Window
 from pyspark.sql import DataFrame, functions as F
 from pyspark.sql.functions import *
 from sparta.logs import getlogger
+from sparta.validator import validator_typed_columns
+from time import time
+from datetime import timedelta
 
 def drop_duplicates(df:DataFrame, col_order: str, cols_partition:List[Any]) -> DataFrame:
     """Function that performs the deletion of duplicate data according to key columns.
@@ -22,14 +25,15 @@ def drop_duplicates(df:DataFrame, col_order: str, cols_partition:List[Any]) -> D
     win = Window.partitionBy(cols_partition).orderBy(F.col(col_order).desc())
     return df.withColumn("col_rank", F.row_number().over(win)).filter(F.col('col_rank') == 1).drop('col_rank')
 
-def aggregation(df:DataFrame, col_order: str, cols_partition: List[str], aggregations:Dict[Any, str]) -> DataFrame:
+def aggregation(df:DataFrame, col_order: str, cols_partition: List[str], aggregations:Dict[Any, Any]) -> DataFrame:
     """This function performs aggregations on columns.
 
     Args:
         df (DataFrame): DataFrame.
         col_order (str): Column to be used in sorting.
         cols_partition (list): List of key columns, for partitioning.
-        aggregations (dict): A dictionary with the columns and the type of aggregation that will be used.
+        aggregations (dict): A dictionary with the columns and the type of aggregation that will be used. 
+        When the same type of aggregation occurs, a list with the column names must be passed.
 
     Returns:
         DataFrame: DataFrame with aggregated columns.
@@ -38,13 +42,47 @@ def aggregation(df:DataFrame, col_order: str, cols_partition: List[str], aggrega
         >>> agg = {F.sum:'new_confirmed', F.first:'order_for_place'}
         >>> cols = ['state', 'city']
         >>> df = aggregation(df, 'date', cols, agg)
+        
+    When the same type of aggregation occurs on different columns.
+    
+    Example:
+        >>> agg = {F.sum:['new_confirmed','order_for_place']}
+        >>> cols = ['state', 'city']
+        >>> df = aggregation(df, 'date', cols, agg)
     """
+    logger = getlogger('aggregation')
+
+    start_time = time()
+
     win = Window.partitionBy(cols_partition).orderBy(F.col(col_order).desc())
+    final_cols = cols_partition
+    cols_partition = tuple(cols_partition)
+    items = set()
+    identificator = 1
     for k in aggregations:
-        df = df.withColumn(aggregations.get(k), k(F.col(aggregations.get(k))).over(win))
-        logger = getlogger('aggregation')
-        logger.info(f'Performed {k} in column {aggregations.get(k)}')
-    return df.withColumn("col_rank", F.row_number().over(win)).filter(F.col('col_rank') == 1).drop('col_rank')
+        if type(aggregations.get(k)) == list:
+            for item in aggregations.get(k):
+                if item in items:
+                    final_cols.append(k(F.col(item)).over(win).alias(f"{item}_{identificator}"))
+                    logger.info(f'Performed {k} in column {item}')
+                    identificator =+1
+                else:
+                    final_cols.append(k(F.col(item)).over(win).alias(item))
+                    logger.info(f'Performed {k} in column {item}')
+                    items.add(item)
+        elif aggregations.get(k) in items:
+            final_cols.append(k(F.col(aggregations.get(k))).over(win).alias(f"{aggregations.get(k)}_{identificator}"))
+            logger.info(f'Performed {k} in column {aggregations.get(k)}')
+            identificator =+1
+        else:
+            final_cols.append(k(F.col(aggregations.get(k))).over(win).alias(aggregations.get(k)))
+            logger.info(f'Performed {k} in column {aggregations.get(k)}')
+            items.add(aggregations.get(k))
+    final_cols.append(F.monotonically_increasing_id().alias('id'))
+    df = df.select(final_cols)
+    win_drop = Window.partitionBy(list(cols_partition)).orderBy(F.col('id').desc())
+    logger.info(f"Execution time: {timedelta(seconds = time()-start_time)}")
+    return df.select("*", F.row_number().over(win_drop).alias("col_rank")).filter(F.col('col_rank') == 1).drop('col_rank').drop('id')
 
 def format_timestamp(df: DataFrame, cols: List[str], timestamp: str = '"yyyy-MM-dd HH:mm:ss"') -> DataFrame:
     """Function that performs a conversion from the date format to a pre-defined timestamp format.
@@ -60,10 +98,14 @@ def format_timestamp(df: DataFrame, cols: List[str], timestamp: str = '"yyyy-MM-
     Example:
         >>> df = format_timestamp(df, ['date'])
     """
+    logger = getlogger('format_timestamp')
+    
+    start_time = time()
+    
     for c in cols:
         df = df.withColumn(c, F.to_timestamp(F.date_format(F.col(c), timestamp), timestamp))
-        logger = getlogger('format_timestamp')
         logger.info(f'Date formatting performed in column {c}')
+    logger.info(f"Execution time: {timedelta(seconds = time()-start_time)}")
     return df
 
 def create_col_list (df: DataFrame, col: str) -> List[str]:
@@ -80,3 +122,31 @@ def create_col_list (df: DataFrame, col: str) -> List[str]:
         >>> create_col_list(df, 'city')
     """
     return [str(value) for value in df.select(col).distinct().rdd.flatMap(lambda x:x).collect()]
+
+def typed_columns(df:DataFrame, typecase:str = 'lower') -> DataFrame:
+    """Function that transforms DataFrame columns into lowercase or uppercase.
+
+    Args:
+        df (DataFrame): DataFrame that will have the columns converted to lowercase or uppercase.
+        typecase (str, optional): The column transformation type can be lower (lowercase) or upper (uppercase).. Defaults to 'lower'.
+
+    Raises:
+        ValueError: If the value of the typecase argument is not upper or lower.
+
+    Returns:
+        DataFrame: DataFrame with columns converted to lowercase or uppercase.
+        
+    Example:
+        >>> typed_columns(df, 'upper')
+    """
+    logger = getlogger('typed_columns')
+    start_time = time()
+    if validator_typed_columns(typecase) == 'Error':
+        raise ValueError(f'This {typecase} is wrong. Typecase values must be lower or upper')
+    if typecase.lower() == 'lower':
+        df = df.toDF(*[c.lower() for c in df.columns])
+    elif typecase.lower() == 'upper':
+        df = df.toDF(*[c.upper() for c in df.columns])
+    logger.info(f'Columns have been changed to {typecase.lower()}')
+    logger.info(f"Execution time: {timedelta(seconds = time()-start_time)}")
+    return df
